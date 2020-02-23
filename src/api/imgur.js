923a9250a85658fd65bded60cd619e3c24738286
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-24 03:41:14 +0700

    :bulb: Fetch Images

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs'
+import Axios from 'axios'
 
 const CLIENT_ID = '363e6430ac09709'
 const ROOT_URL = 'https://api.imgur.com'
@@ -11,5 +12,12 @@ export default {
         }
 
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`
+    },
+    fetchImages(token) {
+        return Axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
     }
 }
