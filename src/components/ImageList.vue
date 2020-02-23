8884345d276d47aedfc1fef7e54531cf30e54283
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-24 04:17:20 +0700

    :bulb: Listing Images

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>ImageList</div>
+  <div class="container">
+    <img v-for="image in allImages" :src="image.link" :key="image.key" />
+  </div>
 </template>
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "ImageList",
+  computed: mapGetters(["allImages"]),
   methods: mapActions(["fetchImages"]),
   created() {
     this.fetchImages();
   }
 };
 </script>
+
+<style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+img {
+  height: 500px;
+}
+</style>
