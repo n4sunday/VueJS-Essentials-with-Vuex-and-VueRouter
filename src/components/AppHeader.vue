babaad7b9cf7c3c845a723229105d54747f05e14
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-24 02:09:24 +0700

    :bulb: Data in Components with MapGetters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
+    {{isLoggedIn}}
     <div class="right menu">
       <a href="#" class="ui item" @click="login">Login</a>
     </div>
@@ -8,14 +9,12 @@
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "AppHeader",
-  methods: {
-    ...mapActions(["login"]),
-    onLogoClick() {}
-  }
+  computed: mapGetters(["isLoggedIn"]),
+  methods: mapActions(["login"])
 };
 </script>
 
