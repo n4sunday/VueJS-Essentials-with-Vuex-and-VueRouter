db80115603276f07d3e54cd61ffe8a00b821cbc0
Sunday <s5935512004@phuket.psu.ac.th>
2020-02-25 05:28:45 +0700

    :bulb: Upload Image API

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur'
+import { router } from '../../main'
 
 const state = {
     images: []
@@ -12,7 +13,17 @@ const actions = {
     async fetchImages({ rootState, commit }) {
         const { token } = rootState.auth
         const response = await api.fetchImages(token)
-        commit('setImages',response.data.data)
+        commit('setImages', response.data.data)
+    },
+    async uploadImages({ rootState }, images) {
+        // Get the access token
+        const { token } = rootState.auth
+
+        // Call our API module to do the upload
+        await api.uploadImages(images, token);
+
+        // Redirect our user to ImageList component
+        router.push('/')
     }
 }
 
